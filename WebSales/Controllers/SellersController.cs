dfcc371d5a9831b1fbcb1a18feeae28e983efa38
Vinícius da Cruz <viniciuscruzcosta011@gmail.com>
2025-09-22 13:24:46 -0300

    Finishing the sellers CRUD

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using WebSales.Services;
 using WebSales.Models;
 using WebSales.Models.ViewModels;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Controllers
 {
@@ -33,6 +34,80 @@ namespace WebSales.Controllers
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- WebSales/Services/SellerService.cs
+++ WebSales/Services/SellerService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using WebSales.Data;
 using WebSales.Models;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Services;
 
@@ -20,5 +22,34 @@ public class SellerService
     {
         _context.Add(obj);
         _context.SaveChanges();
+    }
+
+    public Seller FindById(int id)
+    {
+        return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+    }
+
+    public void Remove(int id)
+    {
+        var obj = _context.Seller.Find(id);
+        _context.Seller.Remove(obj);
+        _context.SaveChanges();
+    }
+
+    public void Update(Seller obj)
+    {
+        if(!_context.Seller.Any(x => x.Id == obj.Id))
+        {
+            throw new NotFoundException("Id not found");
+        }
+        try
+        {
+            _context.Update(obj);
+            _context.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException e)
+        {
+            throw new DbUpdateConcurrencyException(e.Message);
+        }
     }
 }
