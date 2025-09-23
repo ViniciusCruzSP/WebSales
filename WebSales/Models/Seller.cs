f37a95e13b6c01a97f6e2e3191e66d2dcc8e2922
Vinícius da Cruz <vinicius.costa@bib.com.br>
2025-09-23 11:29:12 -0300

    Data input validation

--- WebSales/Models/Seller.cs
+++ WebSales/Models/Seller.cs
@@ -7,16 +7,21 @@ namespace WebSales.Models;
 public class Seller
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "{0} Required")]
+    [StringLength(60, MinimumLength = 5, ErrorMessage = "{0} size should be between {2} and {1}")]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "{0} Required")]
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
-    
+
+    [Required(ErrorMessage = "{0} Required")]
     [Display(Name = "Birth Date")]
     [DataType(DataType.Date)]
-
     public DateTime BirthDate { get; set; }
-    
+
+    [Required(ErrorMessage = "{0} Required")]
+    [Range(100.0, 50000.0, ErrorMessage = "{0} range should be from {2} to {1}")]
     [Display(Name = "Base Salary")]
     [DisplayFormat(DataFormatString = "{0:F2}")]
     public double BaseSalary { get; set; }
