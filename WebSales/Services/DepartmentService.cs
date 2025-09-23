570318ee208bc53cf1533c5be27092b5edc21950
Vinícius da Cruz <vinicius.costa@bib.com.br>
2025-09-23 13:05:13 -0300

    Inserting the async funcionality at SellerService and SellersController

--- WebSales/Services/DepartmentService.cs
+++ WebSales/Services/DepartmentService.cs
@@ -1,6 +1,7 @@
 ﻿using WebSales.Data;
 using WebSales.Models;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebSales.Services
 {
@@ -11,9 +12,9 @@ namespace WebSales.Services
         {
             _context = context;
         }
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
